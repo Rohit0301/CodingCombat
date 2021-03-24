3e3c3bb4f14e28456ad0adf57e34f9026b0fca33
Rohit0301 <rj03012002@gmail.com>
2021-03-24 10:54:43 +0530

    add apps to project setting

--- server/server/settings.py
+++ server/server/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'DataStructures',
+    'Algorithms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

2a481aef8073c90646bd055490c74a07c837d9e3
Rohit0301 <rj03012002@gmail.com>
2021-03-26 12:28:12 +0530

    create urls.py

--- server/server/urls.py
+++ server/server/urls.py
@@ -1,9 +1,9 @@
 """server URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
+    https://d               ocs.djangoproject.com/en/3.1/topics/http/urls/
+    Examples:
+    Function views
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views

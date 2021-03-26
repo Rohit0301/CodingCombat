b3fb51f284d7dfc4ece7fc277c39c1b33c203157
Rohit0301 <rj03012002@gmail.com>
2021-03-26 16:22:50 +0530

    create Algo api view

--- server/Algorithms/views.py
+++ server/Algorithms/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.http.response import JsonResponse
+from rest_framework.parsers import JSONParser 
+from rest_framework import status
+from .serializers import AlgorithmSerializer
+from .models import Algorithm
+from rest_framework.decorators import api_view
 # Create your views here.
+@api_view(['GET'])
+def Algorithms(request):
+    if request.method == 'GET':
+        try: 
+           algorithms = Algorithm.objects.all()
+        except:
+           return HttpResponse(status=404)           
+        algorithms_serializer = AlgorithmSerializer(algorithms, many=True)
+        return JsonResponse(algorithms_serializer.data, safe=False,status=200)

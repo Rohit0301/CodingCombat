acf70f5ee505cc1ca36a3a9ba162054f127b1307
Rohit0301 <rj03012002@gmail.com>
2021-03-26 16:21:45 +0530

    create serializers for all models

--- server/Algorithms/serializers.py
+++ server/Algorithms/serializers.py
@@ -15,5 +15,31 @@ class AlgorithmVideoSerializer(serializers.ModelSerializer):
     class Meta:
         model = AlgorithmsVideo
         fields = ('id',
+                   'Algorithm_Video_Title',
+                   'Algorithm_Video_URL',
+                   'Algorithms_Video_Image',
                   'Algorithm_Name'
                  )
+
+class AlgorithmBlogSerializer(serializers.ModelSerializer):
+ 
+    class Meta:
+        model = AlgorithmsBlog
+        fields = ('id',
+                   'Algorithm_Blog_Title',
+                   'Algorithm_Blog_URL',
+                  'Algorithm_Name'
+                 )
+
+class AlgorithmQuestionSerializer(serializers.ModelSerializer):
+ 
+    class Meta:
+        model = AlgorithmsQuestion
+        fields = ('id',
+                   'Algorithm_Question_Title',
+                   'Algorithm_Question_Link',
+                   'Algorithm_Question_Difficulty',
+                   'Algorithm_Question_Editorial'
+                  'Algorithm_Name'
+                 )
+        

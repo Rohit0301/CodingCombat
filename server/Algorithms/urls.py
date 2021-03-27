f37f9fa8b88a268592ef16db3f6d13bc51bb19e1
Rohit0301 <rj03012002@gmail.com>
2021-03-27 13:06:09 +0530

    add more routes

--- server/Algorithms/urls.py
+++ server/Algorithms/urls.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
-  path("Algorithms/",views.Algorithms,name="Algorithms")
+  path("Algorithms/",views.Algorithms,name="Algorithms"),
+  path("Algorithm/<str:algorithm>/Videos/",views.Algorithmvideos,name="Algorithmvideos"),
+  path("Algorithm/<str:algorithm>/Blogs/",views.Algorithmblogs,name="Algorithmblogs"),
+  path("Algorithm/<str:algorithm>/Questions/",views.Algorithmquestions,name="Algorithmquestions")
 ]

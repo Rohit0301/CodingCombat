9fc00a9df81dcaeddbe57d2d28a225ffbe0da98d
Rohit0301 <rj03012002@gmail.com>
2021-04-10 18:47:22 +0530

    added routes to the project

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
-import Algopage from './Algorithms/AlgoPage/algopage';
+
+import Algopage from './Algorithms/AlgoPage/algoPage';
+import Algohome from './Algorithms/AlgoHome/algoHome';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import Login from './Login/login';
+
 
 class App extends Component {
 
   render(){
+    const history = createBrowserHistory();
     return (
+      
       <React.Fragment>
-          <Algopage/>
-        
+        <Router history={history}>
+         <Switch>
+            <Route path="/Algorithms" exact>
+              <Algohome />
+            </Route>
+            <Route path="/" exact>
+              <Login />
+            </Route>
+            <Route path="/Algorithms/:name" exact>
+              <Algopage />
+            </Route>
+        </Switch> 
+       </Router>        
       </React.Fragment>
     );
   }
--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
+import {Link,BrowserRouter as Router,Route,Switch} from 'react-router-dom';
 ReactDOM.render(
-    <App />,
+  <Router>
+    <App />
+  </Router>,
   document.getElementById('root')
 );
 

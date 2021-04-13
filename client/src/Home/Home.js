9999f50ea157bc34c68b30c9202c1200f39606fc
Rohit0301 <rj03012002@gmail.com>
2021-04-13 14:48:26 +0530

    create home page

---
+++ client/src/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../Navbar/navbar';
+export default function Home (){
+    return(
+        <>
+          <Navbar/>
+          <br>
+          </br>
+          <br></br>
+          <h1>This is home page</h1>
+        </>
+    );
+}

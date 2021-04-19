b522f241f66845c7df5fb53eeb64af6a66745ea9
sargun <sargun.narula5@gmail.com>
2021-04-19 17:50:27 +0530

    uncomment

--- client/src/App.js
+++ client/src/App.js
@@ -15,23 +15,23 @@ import Home from './Home/Home';
 
 
 class App extends Component {
-  // constructor(){
-  //   super();
-  //   this.state={isLogin:false};
-  // }
+  constructor(){
+    super();
+    this.state={isLogin:false};
+  }
   componentDidMount(){
-    // const key=localStorage.getItem("userKey");
-    // if(key==null){
-    //  this.setState({
-    //    isLogin:true
-    //  });
-    // }
-    // else{
-    //   this.setState({
-    //     isLogin:true
-    //   });
-    // }
-    // console.log(this.state.isLogin);
+    const key=localStorage.getItem("userKey");
+    if(key==null){
+     this.setState({
+       isLogin:true
+     });
+    }
+    else{
+      this.setState({
+        isLogin:true
+      });
+    }
+    console.log(this.state.isLogin);
   }
   render(){
     const history = createBrowserHistory();

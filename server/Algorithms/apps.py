03356a5aa687a6b87a123880bcdef1e42cb8bd40
Rohit0301 <rj03012002@gmail.com>
2021-04-19 17:17:42 +0530

    backend deployed

--- server/Algorithms/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AlgorithmsConfig(AppConfig):
-    name = 'Algorithms'
---
+++ server/server/Authentication/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+from allauth.socialaccount.providers.github.views import GitHubOAuth2Adapter
+from allauth.socialaccount.providers.oauth2.client import OAuth2Client
+from rest_auth.registration.views import SocialLoginView,SocialConnectView
+
+
+
+class GoogleLogin(SocialLoginView):
+    adapter_class = GoogleOAuth2Adapter
+
+
+class GithubLogin(SocialLoginView):
+    adapter_class = GitHubOAuth2Adapter
+    callback_url="http://localhost:3000"
+    client_class = OAuth2Client
+

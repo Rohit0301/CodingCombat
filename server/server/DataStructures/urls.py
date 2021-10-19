992ed0b69b7fc00acebc750911aff076cf80b313
sargun <sargun.narula5@gmail.com>
2021-10-19 18:21:51 +0530

    Restrutured Datastructure api to working condition

--- server/server/DataStructures/urls.py
+++ server/server/DataStructures/urls.py
@@ -3,12 +3,9 @@ from django.urls import path
 from . import views
 urlpatterns = [
   path("DataStructures/",views.DataStructures,name="DataStructures"),
-<<<<<<< HEAD:server/server/DataStructures/urls.py
   path("DataStructure/<str:datastructure>/Videos/",views.DataStructurevideos,name="DataStructurevideos"),
   path("DataStructure/<str:datastructure>/Blogs/",views.DataStructureblogs,name="DataStructureblogs"),
-=======
   path("DataStructure/<str:datastructure>/Videos/"   ,views.DataStructurevideos   ,name="DataStructurevideos"),
   path("DataStructure/<str:datastructure>/Blogs/"    ,views.DataStructureblogs    ,name="DataStructureblogs"),
->>>>>>> 6812e3f8001959c69d7711601360969404e16c7e:server/DataStructures/urls.py
   path("DataStructure/<str:datastructure>/Questions/",views.DataStructurequestions,name="DataStructurequestions")
 ]
